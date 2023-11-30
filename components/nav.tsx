43bdce78443641dae5569eab1bf75dea613e8604
Piotr Godzień <piotrgodzien@gmail.com>
2023-11-30 17:56:13 +0100

    Update nav.tsx

--- components/nav.tsx
+++ components/nav.tsx
@@ -68,6 +68,7 @@ export default function Nav() {
                             <div className="space-y-2 py-6">
                                 {navigation.map((item) => (
                                     <a
+                                        onClick={() => setMobileMenuOpen(false)}
                                         key={item.name}
                                         href={item.href}
                                         className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
